cafb8010a28e8244c3817028137eab1250276015
sabbat10 <sabbatlumpatshia99@gmail.com>
2024-07-09 12:20:26 +0100

    add useSat to parent component

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,32 @@ import React from "react";
 import Square from "./components/Square";
 import "./App.css";
 
+import { useState } from "react";
+
 export default function App() {
+  // les useState
+  const [square, setSquare] = useState(Array(9).fill(null));
+
   return (
     <div>
       <h3>Jeu de Morpion</h3>
       <div className="board-row">
         {" "}
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[1]} />
+        <Square value={square[2]} />
+        <Square value={square[3]} />
       </div>
       <div className="board-row">
         {" "}
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[4]} />
+        <Square value={square[5]} />
+        <Square value={square[6]} />
       </div>
       <div className="board-row">
         {" "}
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[7]} />
+        <Square value={square[8]} />
+        <Square value={square[9]} />
       </div>
     </div>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,13 +1,3 @@
-import { useState } from "react";
-export default function Square({ ...props }) {
-  const [value, setValue] = useState(null);
-  function handClick() {
-    setValue("X");
-  }
-
-  return (
-    <button className="square" onClick={handClick}>
-      {value}
-    </button>
-  );
+export default function Square({ value, ...props }) {
+  return <button className="square">{value}</button>;
 }
